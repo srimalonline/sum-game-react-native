aa76c4732d1edb2dd472ec465de2f6bd01abc6e4
Srimal Fernando <dilankasrimal23@gmail.com>
2023-06-25 12:47:15 +0530

    random number generator option

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { View, Text, StyleSheet} from 'react-native';
 
 class Game extends React.Component {
+    target = 10 + Math.floor(40 * Math.random());
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.target}>42</Text>
+                <Text style={styles.target}>{this.target}</Text>
             </View>
         );
     };
